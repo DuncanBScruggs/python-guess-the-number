a12ab4fb7896c8fccdb2bae6633cf6ada1ee904d
DuncanBScruggs <duncanbscruggs@gmail.com>
2021-03-05 12:51:55 -0500

    added computer guesses number user thinks of

--- main.py
+++ main.py
@@ -12,4 +12,21 @@ def guess(x):
 
     print(f"congrats you guess {random_number} correctly")
 
-guess(10)
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C): ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        if feedback == 'l':
+            low = guess + 1
+    print(f'{guess} THATS CORRECT') 
+
+computer_guess(10)
